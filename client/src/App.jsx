ba6eca12bd21930a774a2c0ab8eaa5a2349473ad
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2026-05-30 23:41:57 +0530

    Login page initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from './pages/Login/Login.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <Login />
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import cors from "cors"
 import express from "express"
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
