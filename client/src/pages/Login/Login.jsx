34ab3c9baec24c973274c18464403d679fdb04ca
soumalya kundu <saumalyakundumld@gmail.com>
2026-05-31 00:46:08 +0530

    style

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,27 +3,29 @@ import './Login.css'
 
 function Login() {
   return (
-    <div id="login-container">
-      <form class="form">
-        <p class="form-title">Sign in to your account</p>
+    <>
+      <div id="login-container">
+        <form class="form">
+          <p class="form-title">Sign in to your account</p>
           <div class="input-container">
             <input type="email" placeholder="Enter email" />
             <span>
             </span>
-        </div>
-        <div class="input-container">
+          </div>
+          <div class="input-container">
             <input type="password" placeholder="Enter password" />
           </div>
           <button type="submit" class="submit">
-          Sign in
-        </button>
+            Sign in
+          </button>
 
-        <p class="signup-link">
-          No account?
-          <a href="">Sign up</a>
-        </p>
-    </form>
-   </div>
+          <p class="signup-link">
+            No account?
+            <a href="">Sign up</a>
+          </p>
+        </form>
+      </div>
+    </>
   )
 }
 
