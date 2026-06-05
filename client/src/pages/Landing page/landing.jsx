9d17c312c8cd09d09ad107ca8948718013d3e825
soumalya kundu <saumalyakundumld@gmail.com>
2026-06-05 16:05:58 +0530

    landing page changes

--- client/src/pages/Landing page/landing.jsx
+++ client/src/pages/Landing page/landing.jsx
@@ -1,48 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./landing.css";
+import  "./landing.css";
 
 const Landing = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
+        <div id="landing">
             <nav className="navbar">
                 <div className="logo">ExpenseFlow</div>
-
-                <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-                    ☰
-                </div>
-
-                <ul className={menuOpen ? "nav-links active" : "nav-links"}>
-                    <section>
-                        <li>
-                            <a href="home">Home</a>
-                        </li>
-                    </section>
-
-                    <section>
-                        <li>
-                            <a href="about">About</a>
-                        </li>
-                    </section>
-
-                    <section>
-                        <li>
-                            <a href="services">Services</a>
-                        </li>
-                    </section>
-
-
-                    <section>
-                        <li>
-                            <Link to="/signin" className="contact-btn">
-                            Sign in
-                            </Link>
-                        </li>
-                    </section>
+                <ul className={menuOpen ? "navLinks active" : "navLinks"}>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><Link to="/Signin" className="contactBtn">Sign in</Link></li>
                 </ul>
             </nav>
+
+            <main>
+                <section className="main" id="home">
+                    Home
+                </section>
+                <section className="main" id="about">
+                    About
+                </section>
+                <section className="main" id="services">
+                    Services
+                </section>
+            </main>
+        </div>
         </>
     );
 };
