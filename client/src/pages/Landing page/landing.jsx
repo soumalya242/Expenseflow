270cb15d4d042077fe4453b72e20cac0303c653f
soumalya kundu <saumalyakundumld@gmail.com>
2026-06-06 11:17:16 +0530

    changes

--- client/src/pages/Landing page/landing.jsx
+++ client/src/pages/Landing page/landing.jsx
@@ -14,19 +14,19 @@ const Landing = () => {
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#services">Services</a></li>
-                    <li><Link to="/Signin" className="contactBtn">Sign in</Link></li>
+                    <li><Link to="/signin" className="contactBtn">Sign in</Link></li>
                 </ul>
             </nav>
 
             <main>
                 <section className="main" id="home">
-                    home
+                    
                 </section>
                 <section className="main" id="about">
-                    About
+                    
                 </section>
                 <section className="main" id="services">
-                    Services
+                    
                 </section>
             </main>
         </div>
